a565dfe5dd7a578cadc3cd79be7ed8569fcd9135
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-04 13:25:48 -0600

    Creating an entity and entrypoint for testing

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class GenresController
+    {
+        [HttpGet]
+        public List<Genre> Get()
+        {
+            var repository = new RepositorioEnMemoria();
+            var genres = repository.ObtenerTodosLosGeneros();
+
+            return genres;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ RepositorioEnMemoria.cs
@@ -0,0 +1,23 @@
+﻿using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI
+{
+    public class RepositorioEnMemoria
+    {
+        private List<Genre> _genres;
+
+        public RepositorioEnMemoria()
+        {
+            _genres = new List<Genre>
+            {
+                new Genre {Id = 1, Name = "Comedia"},
+                new Genre {Id = 2, Name = "Acción"},
+            };
+        }
+
+        public List<Genre> ObtenerTodosLosGeneros()
+        {
+            return _genres;
+        }
+    }
+}
