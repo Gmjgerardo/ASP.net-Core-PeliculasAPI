32771b3dc37965b161fde1f4bd3ae0ea603f5d23
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-11 16:46:43 -0600

    Adding actors validations to their CreationDTO

--- DTOs/ActorDTOs.cs
+++ DTOs/ActorDTOs.cs
@@ -5,6 +5,8 @@ namespace PeliculasAPI.DTOs
 {
     public class ActorCreationDTO
     {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
         public required string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public IFormFile? ProfileImage { get; set; }
