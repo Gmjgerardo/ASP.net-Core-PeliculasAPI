d00a6f1b2b0ac76c52bc8353d2bccc32df06f804
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-11 13:42:31 -0600

    Creating actors controller and get by id endpoint

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entities;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/actores")]
+    [ApiController]
+    public class ActorsController: ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly IMapper mapper;
+        private readonly IOutputCacheStore outputCacheStore;
+        private const string cacheTag = "actors";
+
+        public ActorsController(ApplicationDBContext context, IMapper mapper, IOutputCacheStore outputCacheStore)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.outputCacheStore = outputCacheStore;
+        }
+
+        [HttpGet("{id:int}", Name = "obtainActorById")]
+        public void Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] ActorCreationDTO actorCreation)
+        {
+            Actor actor = mapper.Map<Actor>(actorCreation);
+
+            // ToDo: Image managment
+
+            context.Add(actor);
+            await context.SaveChangesAsync();
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            
+            return CreatedAtRoute("obtainActorById", new {id = actor.Id}, actor);
+        }
+
+    }
+}
