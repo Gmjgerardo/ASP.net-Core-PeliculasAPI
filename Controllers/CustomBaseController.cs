0ff983ef10811cfe7a22551e7ce73e8a30f400d4
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-22 23:35:30 -0600

    Creating custom base controller to recycle code implementations in other controllers

---
+++ Controllers/CustomBaseController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entities;
+using PeliculasAPI.Utilities;
+using System.Linq.Expressions;
+
+namespace PeliculasAPI.Controllers
+{
+    public class CustomBaseController: ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly IMapper mapper;
+
+        public CustomBaseController(ApplicationDBContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        protected async Task<List<TDTO>> Get<TEntidad, TDTO>([FromQuery] PaginationDTO pagination,
+            Expression<Func<TEntidad, object>> orderParm)
+            where TEntidad : class
+        {
+            IQueryable<TEntidad> queryable = context.Set<TEntidad>().AsQueryable();
+
+            await HttpContext.InsertPaginationParametersOnHeader(queryable);
+            return await queryable
+                .OrderBy(orderParm)
+                .Paginate(pagination)
+                .Paginate(pagination)
+                .ProjectTo<TDTO>(mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+    }
+}
