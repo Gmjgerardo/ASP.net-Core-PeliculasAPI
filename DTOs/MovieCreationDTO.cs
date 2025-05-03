e0e9448fd1ca056e83b252017175e8850724dbdf
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-05-02 23:43:42 -0600

    Creating movie DTO's

---
+++ DTOs/MovieCreationDTO.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.DTOs
+{
+    public class MovieCreationDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 200, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+        public required string Title { get; set; }
+
+        [Required]
+        public DateOnly ReleaseDate { get; set; }
+
+        public string? Trailer { get; set; }
+
+        [Required]
+        public required IFormFile Image { get; set; }
+    }
+}
---
+++ DTOs/MovieDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace PeliculasAPI.DTOs
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+        public string? Trailer { get; set; }
+        public required string Image { get; set; }
+    }
+}
