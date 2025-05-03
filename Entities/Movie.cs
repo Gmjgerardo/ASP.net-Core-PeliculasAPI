4213826d2b601c68984489dc5ed5418ae3e5b8e0
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-05-02 18:10:06 -0600

    Creating movie entity

---
+++ Entities/Movie.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Entities
+{
+    public class Movie: IId
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 200, ErrorMessage= "El campo {0} debe tener {1} caracteres o menos")]
+        public required string Title { get; set; }
+
+        [Required]
+        public DateOnly ReleaseDate { get; set; }
+
+        public string? Trailer { get; set; }
+
+        [Required]
+        [Unicode(false)]
+        public required string Image { get; set; }
+    }
+}
