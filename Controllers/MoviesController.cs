ad5c77234e489728ff1785e48f0ba70600df2dfa
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-06-05 20:10:09 -0600

    Creating Mvoie Controller with GET and POST endpoints

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entities;
+using PeliculasAPI.Services;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : CustomBaseController
+    {
+        private readonly ApplicationDBContext context;
+        private readonly IMapper mapper;
+        private readonly IOutputCacheStore outputCacheStore;
+        private readonly IFileStorage storage;
+        private const string cacheTag = "movies";
+        private const string container = "movies";
+
+        public MoviesController(ApplicationDBContext context, IMapper mapper,
+            IOutputCacheStore outputCacheStore, IFileStorage storage)
+            : base(context, mapper, outputCacheStore, cacheTag)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.outputCacheStore = outputCacheStore;
+            this.storage = storage;
+        }
+
+        [HttpGet("{id:int}", Name = "obtainMovieById")]
+        public async Task<IActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("PostGet")]
+        public async Task<ActionResult<MoviePostGetDTO>> PostGet()
+        {
+            List<CinemaDTO> cinemas = await context.Cinemas.ProjectTo<CinemaDTO>(mapper.ConfigurationProvider).ToListAsync();
+            List<GenreDTO> genres = await context.Genres.ProjectTo<GenreDTO>(mapper.ConfigurationProvider).ToListAsync();
+
+            return new MoviePostGetDTO
+            {
+                Cinemas = cinemas,
+                Genres = genres,
+            };
+        }
+
+        [HttpGet("{name}")]
+        public async Task<ActionResult<List<MovieActorDTO>>> Get(string name)
+        {
+            return await context.Actors.Where(a => a.Name.Contains(name))
+                .ProjectTo<MovieActorDTO>(mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] MovieCreationDTO movieCreation)
+        {
+            Movie movie = mapper.Map<Movie>(movieCreation);
+
+            if (movieCreation.Image is not null)
+            {
+                string? url = await storage.Storage(container, movieCreation.Image);
+                movie.Image = url;
+            }
+
+            AssignActorsOrder(movie);
+            context.Add(movie);
+
+            await context.SaveChangesAsync();
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+
+            MovieDTO? movieDTO = mapper.Map<MovieDTO>(movie);
+
+            return CreatedAtRoute("obtainMovieById", new { id = movie.Id }, movieDTO);
+        }
+
+        private void AssignActorsOrder(Movie movie)
+        {
+            if (movie.MovieActors is not null)
+            {
+                for (global::System.Int32 i = 0; i < movie.MovieActors.Count; i++)
+                {
+                    movie.MovieActors[i].Order = i;
+                }
+            }
+        }
+    }
+}
